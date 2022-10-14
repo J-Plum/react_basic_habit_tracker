061eba5f7631868b9b1db6fcd4a0a6e248baa248
Euang1812 <bosswjdwo@gmail.com>
2022-10-15 06:53:57 +0900

    add habit tracker

---
+++ src/app.jsx
@@ -0,0 +1,80 @@
+import { Component } from "react";
+import "./app.css";
+import Habits from "./components/Habits";
+import Navbar from "./components/Navbar";
+
+class app extends Component {
+   state = {
+      habits: [
+         { id: 1, name: "Reading", count: 0 },
+         { id: 2, name: "Running", count: 0 },
+         { id: 3, name: "Coding", count: 0 },
+      ],
+   };
+
+   handleIncrement = (habit) => {
+      const habits = this.state.habits.map(item =>{
+         if(item.id === habit.id){
+            return {...habit, count : habit.count + 1}
+         }else{
+            return item;
+         }
+      })
+      this.setState({ habits });
+   };
+
+   handleDecrement = (habit) => {
+      const habits = this.state.habits.map(item =>{
+         if(item.id === habit.id){
+            const count = habit.count -1;
+            return {...habit, count : count<0?0:count}
+         }else{
+            return item;
+         }
+      })
+      this.setState({ habits });
+   };
+
+   handleDelete = (habit) => {
+      const habits = this.state.habits.filter((item) => item.id !== habit.id);
+      this.setState({ habits });
+   };
+
+   handleAdd = (name) => {
+      const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
+      this.setState({ habits });
+   };
+
+   handleReset = () => {
+      const habits = this.state.habits.map(item =>{
+         if(item.count !== 0){
+            return {...item, count : 0}
+         }
+         return item;
+      })
+      this.setState({ habits });
+   };
+
+   render() {
+      console.log('app');
+      return (
+         <>
+            <Navbar
+               totalCount={
+                  this.state.habits.filter((item) => item.count > 0).length
+               }
+            />
+            <Habits
+               habits={this.state.habits}
+               onIncrement={this.handleIncrement}
+               onDecrement={this.handleDecrement}
+               onDelete={this.handleDelete}
+               onAdd={this.handleAdd}
+               onReset={this.handleReset}
+            />
+         </>
+      );
+   }
+}
+
+export default app;
---
+++ src/components/Habits.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Habit from "./Habit";
+import HabitAddForm from "./HabitAddForm";
+
+class Habits extends Component {
+
+   render() {
+      console.log("habits");
+      return (
+         <>
+            <HabitAddForm onAdd={this.props.onAdd} />
+            <ul>
+               {this.props.habits.map((habit) => (
+                  <Habit
+                     key={habit.id}
+                     habit={habit}
+                     onIncrement={this.props.onIncrement}
+                     onDecrement={this.props.onDecrement}
+                     onDelete={this.props.onDelete}
+                  />
+               ))}
+            </ul>
+            <button className="habits-reset" onClick={this.props.onReset}>
+               Reset All
+            </button>
+         </>
+      );
+   }
+}
+
+export default Habits;
